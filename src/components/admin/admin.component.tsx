56b174ed168adf3b6e13b511d435b1cbb982bc8d
Tomivan <gracejunkie20@gmail.com>
2023-05-11 23:45:41 +0100

    added and styled admin components

--- src/components/admin/admin.component.tsx
+++ src/components/admin/admin.component.tsx
@@ -3,18 +3,26 @@ import "./admin.css";
 
 const Admin = () => {
     return(
-        <div className="">
-            <table>
+        <div className="p-4">
+            <button className="position: absolute right-4 bg-purple-800 text-white rounded w-1/6 h-10">Register client</button>
+            <table className="position: absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-gray border-solid border-2">
                 <thead>
-                    <tr>
-                        <th>Number</th>
-                        <th>Name</th>
+                    <tr className="">
+                        <th className="p-4 border-gray border-solid border-2">Number</th>
+                        <th className="p-4 border-gray border-solid border-2">Name</th>
+                        <th className="p-4 border-gray border-solid border-2">Phone Number</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>48</td>
-                        <td>Name</td>
+                    <tr className="">
+                        <td className="p-4 border-gray border-solid border-2">48</td>
+                        <td className="p-4 border-gray border-solid border-2">Eden Hazard</td>
+                        <td className="p-4 border-gray border-solid border-2">07036358412</td>
+                    </tr>
+                    <tr className="">
+                        <td className="p-4 border-gray border-solid border-2">48</td>
+                        <td className="p-4 border-gray border-solid border-2">Eden Hazard</td>
+                        <td className="p-4 border-gray border-solid border-2">07036358412</td>
                     </tr>
                 </tbody>
             </table>
--- src/components/queue/queue.component.tsx
+++ src/components/queue/queue.component.tsx
@@ -3,14 +3,12 @@ import "./queue.css";
 
 const Queue = () => {
     return(
-        <div className="">
-            <div className="">
-                <p>Zlatan Ibrahimovic</p>
-                <p>zlatanibrahimovic@gmail.com</p>
-                <p>07004500523</p>
-                <p>No. 48</p>
-                <button>Leave Queue</button>
-            </div>
+        <div className="p-4 position: absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <p><span className="text-blue-500">Name: </span>Zlatan Ibrahimovic</p>
+            <p><span className="text-blue-500">Email: </span>zlatanibrahimovic@gmail.com</p>
+            <p><span className="text-blue-500">Number: </span>07004500523</p>
+            <p><span className="text-blue-500">Queue No: </span> 48</p>
+            <button className="mt-4 bg-purple-800 text-white rounded w-4/5 h-10">Leave Queue</button>
         </div>
     )
 }

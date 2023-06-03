b1683df47883bd8bb4498efde42bfa9cbfe22502
samtaylek17 <ogunsusitemitayo99@gmail.com>
2023-06-03 19:17:18 +0100

    Duro MVP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UserInfoPage from './user-info';
 import Queue from './queue';
 import AdminPage from './admin';
-import Login from './login';
 import AdminSignup from './admin-signup';
 
 const Pages = () => {
@@ -13,7 +12,6 @@ const Pages = () => {
 				<Route path='/' element={<UserInfoPage />} />
 				<Route path='/queue' element={<Queue />} />
 				<Route path='/admin/:id' element={<AdminPage />} />
-				<Route path='/admin-login' element={<Login />} />
 				<Route path='/admin-signup' element={<AdminSignup />} />
 			</Routes>
 		</Router>
